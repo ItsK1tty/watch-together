a26aed15285308979d167442d42a12ca995715ce
ItsK1tty <tb03@daftpenk.com>
2022-10-04 20:45:32 +0300

    multiclient?

--- coordinationServer.py
+++ coordinationServer.py
@@ -1,30 +1,9 @@
 import socket
 import ssl
+import _thread
 
-MAX_CLIENT = 10
-
-state = ["Hello", "Auth", "AuthSuccess" "InvalidAuth", "WatchJoin", "Watching", "Terminated"]
-testCreds = [["test", "test"]]
-def server_program():
-    # get the hostname
-    host = "localhost" #socket.gethostname()
-    port = 12545  # initiate port no above 1024
-
-    server_socket = socket.socket()  # get instance
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    server_socket = ssl.wrap_socket(
-    server_socket, server_side=True, keyfile="server.key", certfile="server.crt"
-)
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(MAX_CLIENT)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
+def on_new_client(conn,addr):
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
         if not data:
             # if data is not received break
@@ -49,10 +28,36 @@ def server_program():
         elif data == "bye":
             curState = state[6]
             conn.send("bye".encode())
-
-        #conn.send(data.encode())  # send data to the client
-
     conn.close()  # close the connection
+
+MAX_CLIENT = 10
+
+state = ["Hello", "Auth", "AuthSuccess" "InvalidAuth", "WatchJoin", "Watching", "Terminated"]
+testCreds = [["test", "test"]]
+def server_program():
+    # get the hostname
+    host = "localhost" #socket.gethostname()
+    port = 12545  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket = ssl.wrap_socket(
+    server_socket, server_side=True, keyfile="server.key", certfile="server.crt"
+)
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(MAX_CLIENT)
+    
+    
+    while True:
+        conn, address = server_socket.accept()  # accept new connection
+        print("Connection from: " + str(address))
+        _thread.start_new_thread(on_new_client,(conn,address))
+    server_socket.close()
+
 
 
 if __name__ == '__main__':
